c5bad394655a9b96081f38b08001a5ce5a89bfcf
Alexis Comte <alexis.comte73@icloud.com>
2024-05-19 13:48:21 +0200

    [router] adding loader function for user info when first loading

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'virtual:uno.css'
 import { RouterProvider } from 'react-router-dom'
-import { createRouter } from './router.tsx'
+import { createRouter, store } from './router.tsx'
+import { Provider } from 'react-redux'
 
 const router = createRouter()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 )
--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,15 @@
 import { createBrowserRouter } from "react-router-dom"
 import HomePage from "./pages/HomePage"
+import { createStore } from "./lib/create-store"
+import { InMemoryUserRepository } from "./lib/auth/infra/in-memory-user.repository"
+import { getMeThunk } from "./lib/auth/usecases/get-me.usecase"
+
+const userRepository = new InMemoryUserRepository()
+export const store = createStore({userRepository}, {})
+
+const getMeLoader = async () => {
+  return store.dispatch(getMeThunk())
+}
 
 export const createRouter = () => {
   return createBrowserRouter([{
@@ -7,7 +17,8 @@ export const createRouter = () => {
     children: [{
       path: "/",
       index: true,
-      Component: HomePage
+      Component: HomePage,
+      loader: getMeLoader
     }]
   }])
 }
