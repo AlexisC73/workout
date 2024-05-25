3722a779b7ec4b170cdbe013767ba45742218b3a
Alexis Comte <alexis.comte73@icloud.com>
2024-05-25 13:52:44 +0200

    [style] adding reset css

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '@unocss/reset/tailwind.css'
 import 'virtual:uno.css'
 import { RouterProvider } from 'react-router-dom'
 import { createRouter, store } from './router.tsx'
