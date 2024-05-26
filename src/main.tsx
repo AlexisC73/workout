e6ca8681bd298f310460ae73657e4a05b436e313
Alexis Comte <alexis.comte73@icloud.com>
2024-05-26 10:19:34 +0200

    adding font

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import '@unocss/reset/tailwind.css'
 import 'virtual:uno.css'
+import "./index.css"
 import { RouterProvider } from 'react-router-dom'
 import { createRouter, store } from './router.tsx'
 import { Provider } from 'react-redux'
