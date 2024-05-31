d00df6149ab686d3ad97a6fbdd63df76a6572018
Alexis Comte <alexis.comte73@icloud.com>
2024-05-31 15:54:47 +0200

    fix link button size

--- src/components/ui/button/link-button.tsx
+++ src/components/ui/button/link-button.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { ReactNode } from "react";
 
 export default function LinkButton ({children, href, style = "primary"}: {children: ReactNode, href: string, style?: "primary" | "secondary" | "tertiary"}) {
-  const defaultButtonStyle = "rounded-2 font-medium p-4 w-full"
+  const defaultButtonStyle = "rounded-2 font-medium p-4 flex justify-center"
   const primaryButtonStyle = "bg-blue-6 text-white"
   const secondaryButtonStyle = "border-blue-6 border text-blue-6"
   const tertiaryButtonStyle = "text-blue-6 bg-blue-1"
