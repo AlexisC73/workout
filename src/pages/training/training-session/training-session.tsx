c2480d961eee3c5f1663c479877e8267081a1bbd
Alexis Comte <alexis.comte73@icloud.com>
2024-05-31 16:26:46 +0200

    add empty session view

--- src/pages/training/training-session/training-session.tsx
+++ src/pages/training/training-session/training-session.tsx
@@ -1,4 +1,4 @@
-import { DotsIcon } from "@/assets/icons"
+import { DotsIcon, NotesEmptySVG } from "@/assets/icons"
 import SearchBar from "@/components/ui/searchbar/searchbar"
 import Layout from "@/layout"
 
@@ -6,12 +6,12 @@ export default function TrainingSessionPage () {
   return (
     <Layout.WithHeader>
       <div className="flex flex-1">
-        <div className="w-100 px-8 pt-10 border-r hidden lg:block">
+        <div id="left-col" className="w-100 px-8 pt-10 border-r hidden lg:block">
           <div>
             <SearchBar />
           </div>
         </div>
-        <div className="px-8 flex-1 pt-8 max-w-245 mx-auto">
+        <div id="right-col" className="px-8 flex flex-col flex-1 pt-8 max-w-245 mx-auto">
           <div className="lg:hidden">
             <SearchBar />
           </div>
@@ -19,8 +19,20 @@ export default function TrainingSessionPage () {
             <h1 className="text-8">Votre séance</h1>
             <DotsIcon className="text-7" />
           </div>
+          <div id="exercices" className="flex flex-col gap-y-10 flex-1">
+            <EmptyTrainingSession />
+          </div>
         </div>
       </div>
     </Layout.WithHeader>
+  )
+}
+
+function EmptyTrainingSession () {
+  return (
+    <div className="flex flex-1 mt-20 lg:mt-0 items-center lg:justify-center flex-col gap-y-10 lg:gap-y-20">
+      <NotesEmptySVG className="text-50 lg:text-70" />
+      <p className="font-medium">Votre séance est vide, recherchez un éxercice afin de l’ajouter.</p>
+    </div>
   )
 }
