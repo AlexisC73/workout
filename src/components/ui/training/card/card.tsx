690bdac8b919f5ed7fdf239dcc61471a2a03f7e1
Alexis Comte <alexis.comte73@icloud.com>
2024-05-31 15:37:58 +0200

    adding options icon for card

--- src/components/ui/training/card/card.tsx
+++ src/components/ui/training/card/card.tsx
@@ -1,7 +1,12 @@
+import { DotsIcon } from "@/assets/icons";
+
 export default function TrainingCard ({name, exercices}: {name: string, exercices: string}) {
   return (
     <div className="bg-white p-4 border rounded-2 flex flex-col gap-y-3 shadow">
-      <h3 className="text-5 font-semibold">{name}</h3>
+      <div className="flex justify-between items-center">
+        <h3 className="text-5 font-semibold hover:text-blue-6 cursor-pointer">{name}</h3>
+        <DotsIcon className="text-8 cursor-pointer" />
+      </div>
       <p className="text-4 text-justify">{exercices}</p>
     </div>
   )
